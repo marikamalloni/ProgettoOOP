586d5c711e6195f73d745c001a2d4a77e613be1a
martinasbrega <s1095471@studenti.univpm.it>
2022-02-21 15:26:30 +0100

    setup spring initializr

---
+++ TweetGeo/src/test/java/it/univpm/TweetGeo/TwitterGeoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.TweetGeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterGeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
